860cf51158eb8fc6c0e11c9fac291c7be7b70a82
Ahmed El-Gaafrawy <ahmed.elgaafrawy111@gmail.com>
2020-04-12 22:09:53 +0200

    New Application

--- APP/main.c
+++ APP/main.c
@@ -19,20 +19,30 @@ void fun  (void*);
 void fun1 (void*);
 void fun2 (void*);
 
+struct d
+{
+	u8 h;
+	u8 g;
+};
+
 int main (void)
 {
+	DDRA=0xff;
+	struct d k ={123};
+	void*ptr=&k;
+
 	PORTD|=(1<<2);
 	PORTD|=(1<<3);
-
 	DDRB|=(7<<4);
+
 	GIE_enuEnable();
 	EXT_INT_enuInit();
 	EXT_INT_enuSelectSenceLevel(ANY_LOGICAL_CHANGE_INT0);
 	EXT_INT_enuSelectSenceLevel(RISING_EDAGE_INT1);
 	EXT_INT_enuSelectSenceLevel(FALLING_EDAGE_INT2);
 	EXT_INT_enuCallBackFunction(fun , NULL , INT0);
-	EXT_INT_enuCallBackFunction(fun1 , NULL , INT1);
-	EXT_INT_enuCallBackFunction(fun2 , NULL , INT2);
+	EXT_INT_enuCallBackFunction(fun1 , ptr , INT1);
+	EXT_INT_enuCallBackFunction(fun2 , ptr , INT2);
 
 	while(1)
 	{
@@ -48,8 +58,14 @@ void fun (void* para)
 void fun1 (void* para)
 {
 	PORTB^=(1<<5);
+	struct  d *p=(struct d*)para;
+	PORTA= p->h;
+	p->g=15;
 }
 void fun2 (void* para)
 {
 	PORTB^=(1<<6);
+	struct  d *p=(struct d*)para;
+	PORTA= p->g;
+
 }
